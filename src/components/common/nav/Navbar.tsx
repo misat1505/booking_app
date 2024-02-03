d94bb92e0ddcfc53b2a19ce075710fccc0c80432
misat1505 <misat2002@gmail.com>
2024-02-03 13:45:53 +0100

    navbar uses shadcn

--- src/components/common/nav/Navbar.tsx
+++ src/components/common/nav/Navbar.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { useUserContext } from "@/app/contexts/userContext";
-import { Avatar } from "flowbite-react";
 import { useState } from "react";
 import NavbarSidebar from "./NavbarSidebar";
 import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FaUser } from "react-icons/fa6";
 
 export default function Navbar() {
   const { user } = useUserContext();
@@ -13,17 +14,23 @@ export default function Navbar() {
     <>
       <header className="fixed w-full top-0 flex justify-between items-center p-6 bg-slate-200 z-50">
         <Link href={"/"}>
-          <Avatar img={"/logo.avif"} rounded className="hover:cursor-pointer" />
+          <Avatar>
+            <AvatarImage src="/logo.avif" />
+            <AvatarFallback>logo</AvatarFallback>
+          </Avatar>
         </Link>
         <div className="flex gap-4 items-center">
           <div>{user?.displayName}</div>
           <Avatar
-            img={user?.photoURL}
-            rounded
-            title={openSidebar ? "close sidebar" : "open sidebar"}
-            className="hover:cursor-pointer"
             onClick={() => setOpenSidebar((prev) => !prev)}
-          />
+            className="hover:cursor-pointer"
+            title={openSidebar ? "close sidebar" : "open sidebar"}
+          >
+            <AvatarImage src={user?.photoURL} />
+            <AvatarFallback>
+              <FaUser size={30} className="text-slate-600" />
+            </AvatarFallback>
+          </Avatar>
         </div>
         <NavbarSidebar
           openSidebar={openSidebar}
